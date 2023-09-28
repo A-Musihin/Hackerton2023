e9ad2d8601a24f95611b5431bacc850e3ab9e719
Julian <julianschweder@gmail.com>
2023-09-28 11:35:24 +0200

    Update app.py

--- code/python/app.py
+++ code/python/app.py
@@ -1,10 +1,64 @@
-from flask import Flask
+from flask import Flask, render_template, request, redirect, url_for
+frome flask_login import LoginManager, UserMixin, login_user, login_required, logout_user, current_user
 
 app = Flask(__name__)
 
+login_manager = LoginManager()
+login_manager.init_app(app)
+
+class User(UserMixin):
+    def __init__(self, id, username, email)
+        self.id = id
+        self.username = username
+        self.email = email
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    if request.method == 'POST':
+        username = request.form['username']
+        email = request.form['email']
+        password = request.form['password']
+
+        user = User(1, username, email)
+
+        user.password_hash = generate_password_hash(password)
+
+        db.session.add(user)
+        db.session.commit()
+
+        login_user(user)
+
+        return redirect(url_for('index'))
+
+    return render_template('register.html')
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+
+        user = db.session.query(User).filter(User.username == username).first()
+
+        if user and check_password_hash(user.password_hash, password):
+            login_user(user)
+            return redirect(url_for('index'))
+        
+        return render_template('login.html')
+
+    return render_template('login.html')        
+
+@app.route('/logout')
+def logout():
+    logout_user()
+    return redirect(url_for('index'))
+
 @app.route('/')
-def hello_world():
-    return 'Hello, World!'
+@login_required
+def index():
+    return render_template('index.html')
+
+
 
 if __name__ == '__main__':
     app.run(debug=True)
