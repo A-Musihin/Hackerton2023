f6a6a0b3e9f9f4a26e7c446f75e5f1cd2f1e001a
A-Musihin <122558845+A-Musihin@users.noreply.github.com>
2023-09-28 14:27:07 +0200

    first defentions created

--- db/db_create.sql
+++ db/db_create.sql
@@ -1,1 +1,40 @@
-/* Placeholder für Datenbank Create */
+/*Creates Database "dbx" with Tables */
+
+CREATE DATABASE IF NOT EXISTS dbx;
+USE dbx;
+
+CREATE TABLE IF NOT EXISTS credentials (
+	 CredID INT PRIMARY KEY AUTO_INCREMENT,	
+    UserID INT,
+    UserPW VARCHAR(64)
+);
+
+CREATE TABLE IF NOT EXISTS users (
+    UserID INT PRIMARY KEY AUTO_INCREMENT,
+    UserFirstName VARCHAR(50),
+    UserLastName VARCHAR(50),
+    UserType SET('admin','teacher','student') NOT NULL
+);
+
+/*Tabelle 'usertype' obsolete da eigenes Feld in Users Tabelle*/
+/*
+CREATE TABLE IF NOT EXISTS usertype (
+    UserTypeID SET ('admin','teacher','student') PRIMARY KEY NOT NULL,
+    Description VARCHAR(50)
+);
+*/
+
+/*Fremdschlüsselzuweisung*/
+
+ALTER TABLE credentials
+ADD CONSTRAINT FK_USER_CREDS
+FOREIGN KEY (UserID)
+REFERENCES users (UserID);
+
+/*OBSOLETE da eigenes Feld auf Users Tabelle*/
+/*
+ALTER TABLE users
+ADD CONSTRAINT FK_USER_USERTYPE
+FOREIGN KEY (UserType)
+REFERENCES UserType (UserTypeID);
+*/
--- db/db_insert.sql
+++ db/db_insert.sql
@@ -1,1 +1,14 @@
-/* Placeholder für Datenbank Insert */
+/* Fills Database "dbx" with Table Data */
+/* PWs encrypted in SHA256 */
+
+USE dbx;
+
+INSERT IGNORE INTO Credentials (UserID, UserPW)
+VALUES (1,'5994471abb01112afcc18159f6cc74b4f511b99806da59b3caf5a9c173cacfc5');
+
+INSERT IGNORE INTO User (UserID, UserFirstName, UserLastName, UserType)
+VALUES (1,'Long','Johnson','admin');
+
+INSERT IGNORE INTO UserType (UserTypeID, Description)
+VALUES (1,'Prinzenrolle');
+
