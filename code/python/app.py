73bd7ce38e86bfa5b0a7f8c58536586d3787bfaa
Julian <julianschweder@gmail.com>
2023-09-28 11:36:18 +0200

    Update app.py

--- code/python/app.py
+++ code/python/app.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template, request, redirect, url_for
-frome flask_login import LoginManager, UserMixin, login_user, login_required, logout_user, current_user
+from flask_login import LoginManager, UserMixin, login_user, login_required, logout_user, current_user
 
 app = Flask(__name__)
 
