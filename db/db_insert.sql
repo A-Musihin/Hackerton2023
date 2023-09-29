ab3f7c0365d378da56e52253771ace71802ef076
A-Musihin <122558845+A-Musihin@users.noreply.github.com>
2023-09-29 12:48:35 +0200

    added db_operations,modifed create/insert

--- db/db_insert.sql
+++ db/db_insert.sql
@@ -3,12 +3,20 @@
 
 USE dbx;
 
-INSERT IGNORE INTO Credentials (UserID, UserPW)
+INSERT IGNORE INTO credentials (UserID, UserPW)
 VALUES (1,'5994471abb01112afcc18159f6cc74b4f511b99806da59b3caf5a9c173cacfc5');
 
-INSERT IGNORE INTO User (UserID, UserFirstName, UserLastName, UserType)
+INSERT IGNORE INTO users (UserID, UserFirstName, UserLastName, UserType)
 VALUES (1,'Long','Johnson','admin');
+
+INSERT 
+
+
+/* SQL-QUERY GRAVEYARD*/ 
+/*
 
 INSERT IGNORE INTO UserType (UserTypeID, Description)
 VALUES (1,'Prinzenrolle');
 
+*/
+/**/
