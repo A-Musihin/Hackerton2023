024dc467672f2b76d45c9811230c18a1a444c70b
Julian <julianschweder@gmail.com>
2023-10-05 10:41:19 +0200

    Update dbconnector.py

--- code/python/dbconnector.py
+++ code/python/dbconnector.py
@@ -1,6 +1,6 @@
 import mysql.connector
 
-#Klassen zum Verbinden mit der Datenbank und zum Ausführen von Queries 
+# Klassen zum Verbinden mit der Datenbank und zum Ausführen von Queries 
 class DatabaseConnector:
     def __init__(self, host, user, password, database):
         self.host = host
@@ -25,9 +25,23 @@ class DatabaseConnector:
         if self.connection:
             self.connection.close()
 
-    def execute_query(self, query, values):
+    def execute_query(self, query, params=None):
         cursor = self.connection.cursor()
-        cursor.execute(query, values)
-        result = cursor.fetchone()
+        cursor.execute(query, params)
+        result = cursor.fetchall()
         cursor.close()
         return result
+
+    def insert_user(self, username, password):
+        query = "INSERT INTO users (username, password) VALUES (%s, %s)"
+        params = (username, password)
+        self.execute_query(query, params)
+
+    def get_user(self, username):
+        query = "SELECT * FROM users WHERE username = %s"
+        params = (username,)
+        result = self.execute_query(query, params)
+        if len(result) > 0:
+            return result[0]
+        else:
+            return None
