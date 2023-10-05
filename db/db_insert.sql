1cca7c5d21c96645d4d0c7ca58f381979777b68e
Julian <julianschweder@gmail.com>
2023-10-05 10:24:22 +0200

    Update db_insert.sql

--- db/db_insert.sql
+++ db/db_insert.sql
@@ -1,1 +1,6 @@
-/* Placeholder für Datenbank Insert */
+USE user_data;
+
+INSERT INTO users (username, email) VALUES ('johndoe','johndoe@email.com');
+
+INSERT INTO password (user_id, password) VALUES (1, 'test123');
+
