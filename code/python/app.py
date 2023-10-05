6e861b17c900c24393e739bcfd988ee0e0bedf87
Julian <julianschweder@gmail.com>
2023-10-05 10:40:45 +0200

    update basically everything

--- code/python/app.py
+++ code/python/app.py
@@ -5,78 +5,31 @@ from forms import LoginForm, RegistrationForm
 from dbconnector import DatabaseConnector
 
 app = Flask(__name__)
-app.secret_key = 'your_secret_key_here'
 
-login_manager = LoginManager()
-login_manager.init_app(app)
+app.config['SECRET_KEY'] = 'e899d4a5c8f35f32fe47edb9620d1b8b'
 
-class User(UserMixin):
-    pass
 
-@login_manager.user_loader
-def load_user(user_id):
-    db = DatabaseConnector('localhost', 'root', '', 'flask')
-    db.connect()
-    query = "SELECT * FROM users WHERE id = %s"
-    values = (user_id,)
-    result = db.execute_query(query, values)
-    db.close()
-    if result:
-        user = User()
-        user.id = result[0]['id']
-        user.username = result[0]['username']
-        user.email = result[0]['email']
-        user.password_hash = result[0]['password']
-        return user
-    else:
-        return None
-    
-app.route('/login', methods=['GET', 'POST'])
-def login():
-    if current_user.is_authenticated:
-        return redirect(url_for('index'))
-
-    form = LoginForm()
-    if form.validate_on_submit():
-        db = DatabaseConnector('localhost', 'root', '', 'flask')
-        db.connect()
-        query = "SELECT * FROM users WHERE email = %s"
-        values = (form.email.data,)
-        result = db.execute_query(query, values)
-        db.close()
-        if result and check_password_hash(result[0]['password'], form.password.data):
-            user = User()
-            user.id = result[0]['id']
-            user.username = result[0]['username']
-            user.email = result[0]['email']
-            login_user(user)
-            return redirect(url_for('index'))
-        else:
-            flash('Invalid email or password')
-    return render_template('login.html', form=form)
-
-@app.route('/logout')
-@login_required
-def logout():
-    logout_user()
-    return redirect(url_for('index'))
-
+# login and registration
 @app.route('/register', methods=['GET', 'POST'])
 def register():
-    if current_user.is_authenticated:
-        return redirect(url_for('index'))
-
     form = RegistrationForm()
     if form.validate_on_submit():
-        db = DatabaseConnector('localhost', 'root', '', 'flask')
-        db.connect()
-        password_hash = generate_password_hash(form.password.data)
-        query = "INSERT INTO users (username, email, password) VALUES (%s, %s, %s)"
-        values = (form.username.data, form.email.data, password_hash)
-        db.execute_query(query, values)
-        db.close()
-        return redirect(url_for('login'))
-    return render_template('register.html', form=form)
+        flash(f'Account created for {form.username.data}!', 'success')
+        return redirect(url_for('index'))
+    return render_template('register.html', title='Register', form=form)
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        if form.email.data == 'admin@blog.com' and form.password.data == '123':
+            flash('You have been logged in!', 'success')
+            return redirect(url_for('tables'))
+
+        else:
+            flash('Login Unsuccessful. Please check username and password', 'danger')
+    return render_template('login.html', title='Login', form=form)
 
 @app.route('/')
 @login_required
