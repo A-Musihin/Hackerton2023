f27ae24e74f06cc633675e24109637850b3b19a5
Julian <julianschweder@gmail.com>
2023-10-05 10:56:31 +0200

    Update forms.py

--- code/python/forms.py
+++ code/python/forms.py
@@ -1,10 +1,33 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField, BooleanField
-from wtforms.validators import DataRequired, Length, EqualTo, email_validator
+from wtforms.validators import DataRequired, Length, EqualTo, Email
 from dbconnector import DatabaseConnector
 
+
+class LoginForm(FlaskForm):
+    email = StringField('Email', validators=[DataRequired(), Email()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    remember = BooleanField('Remember Me')
+    submit = SubmitField('Login')
+
+    def validate(self):
+        if not super(LoginForm, self).validate():
+            return False
+
+        db = DatabaseConnector('localhost', 'root', 'password', 'mydatabase')
+        db.connect()
+        user = db.get_user(self.email.data)
+        db.close()
+
+        if user is None or user['password'] != self.password.data:
+            self.email.errors.append('Invalid email or password')
+            return False
+
+        return True
+
+
 class RegistrationForm(FlaskForm):
-    email = StringField('Email', validators=[DataRequired(), email_validator()])
+    email = StringField('Email', validators=[DataRequired(), Email()])
     password = PasswordField('Password', validators=[DataRequired()])
     confirm_password = PasswordField('Confirm Password', validators=[DataRequired(), EqualTo('password')])
     submit = SubmitField('Sign Up')
@@ -25,26 +48,3 @@ class RegistrationForm(FlaskForm):
         db.close()
 
         return True
-
-
-class LoginForm(FlaskForm):
-    email = StringField('Email', validators=[DataRequired(), email_validator()])
-    password = PasswordField('Password', validators=[DataRequired()])
-    remember = BooleanField('Remember Me')
-    submit = SubmitField('Login')
-
-    def validate(self):
-        if not super(LoginForm, self).validate():
-            return False
-
-        db = DatabaseConnector('localhost', 'root', 'password', 'mydatabase')
-        db.connect()
-        user = db.get_user(self.email.data)
-        db.close()
-
-        if user is None or user['password'] != self.password.data:
-            self.email.errors.append('Invalid email or password')
-            return False
-
-        return True
-
