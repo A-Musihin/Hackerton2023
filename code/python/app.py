0b26eb3836d8f82285674742a2b1ea8e7081bd14
Julian <julianschweder@gmail.com>
2023-10-06 09:52:50 +0200

    Update app.py

--- code/python/app.py
+++ code/python/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, redirect, url_for, flash
+from flask import Flask, render_template, redirect, url_for, flash, request
 from flask_login import LoginManager, UserMixin, login_user, login_required, logout_user 
 from werkzeug.security import generate_password_hash, check_password_hash
 from forms import LoginForm, RegistrationForm
