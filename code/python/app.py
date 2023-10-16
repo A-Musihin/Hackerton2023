c90ce4f1c2ef0ed4d8fe87756f9ba9a660a5ee16
DRKBY <71647508+DRKBY@users.noreply.github.com>
2023-10-16 19:09:00 +0200

    update app.py forms.py add requirements.txt

--- code/python/app.py
+++ code/python/app.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template, request, redirect, url_for, flash
-from flask_login import LoginManager, UserMixin, login_user, login_required, logout_user, current_user 
+from flask_login import LoginManager, UserMixin, login_user, login_required, logout_user
 from werkzeug.security import generate_password_hash, check_password_hash
 from forms import LoginForm, RegistrationForm
 from dbconnector import DatabaseConnector
